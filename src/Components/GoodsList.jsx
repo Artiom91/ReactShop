9f2d28d627ea1fd133a1e388f7b95a15548cf631
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-04-27 15:08:46 +0300

    add shoping cart

--- src/Components/GoodsList.jsx
+++ src/Components/GoodsList.jsx
@@ -2,6 +2,14 @@ import { GoodsItem } from "./GoodsItem"
 
 function GoodsList(props) {
     const {goods = []} = props
+
+    if(!goods.length) {
+        return <h3>Nothing Here</h3>
+    }
+
+    return (<div className="goods">
+        {goods.map((item) => (<GoodsItem key={item.id} {...item}/>))}
+    </div>)
 }
 
 export { GoodsList }
--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react'
 import { API_KEY, API_URL } from '../config'
 import { Preloader } from './Preloader'
+import { GoodsList } from './GoodsList'
+import { Cart } from './Cart'
 
 function Shop() {
   const [goods, setGoods] = useState([])
   const [loading, setLoading] = useState(true)
+  const [order, setOrder] = useState([])
 
   useEffect(function getGoods() {
     fetch(API_URL, {
@@ -19,9 +22,12 @@ function Shop() {
       })
   }, [])
 
-  return <main className="container content">{
-    loading ? <Preloader /> : goods
-  }</main>
+  return (
+    <main className="container content">
+      <Cart quantity={order.length} />
+      {loading ? <Preloader /> : <GoodsList goods={goods} />}
+    </main>
+  )
 }
 
 export { Shop }
