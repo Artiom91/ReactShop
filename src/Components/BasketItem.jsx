c00f0d28d47d718bd178952b3fd3ddf00d2c968b
Artiom <102260968+Artiom91@users.noreply.github.com>
2023-05-07 17:36:07 +0300

    add dec and inc quantity

--- src/Components/BasketItem.jsx
+++ src/Components/BasketItem.jsx
@@ -1,11 +1,27 @@
 function BasketItem(props) {
-  const { id, name, price, quantity } = props
+  const {
+    id,
+    name,
+    price,
+    quantity,
+    removeFromBasket = Function.prototype,
+    incQuantity = Function.prototype,
+    decQuantity = Function.prototype,
+  } = props
 
   return (
     <li className="collection-item">
-      {name} x{quantity} = {price}
+      {name} <i className="material-icons basket-quantity" onClick={() => {decQuantity(id)}}>remove</i> x
+      {quantity} <i className="material-icons basket-quantity" onClick={() => {incQuantity(id)}}>add</i> ={' '}
+      {price * quantity} руб.
       <span class="secondary-content">
-        <i class="material-icons basket-delete">clear</i>
+        <i
+          class="material-icons basket-delete"
+          onClick={() => {
+            removeFromBasket(id)
+          }}>
+          clear
+        </i>
       </span>{' '}
     </li>
   )
--- src/Components/BasketList.jsx
+++ src/Components/BasketList.jsx
@@ -1,7 +1,13 @@
 import { BasketItem } from './BasketItem'
 
 function BasketList(props) {
-  const { order, handleBasketShow = Function.prototype } = props
+  const {
+    order,
+    handleBasketShow = Function.prototype,
+    removeFromBasket = Function.prototype,
+    incQuantity,
+    decQuantity,
+  } = props
   const totalPrice = order.reduce((sum, el) => {
     return sum + el.price * el.quantity
   }, 0)
@@ -10,12 +16,22 @@ function BasketList(props) {
     <ul className="collection basket-list">
       <li className="collection-item active">Корзина</li>
       {order.length ? (
-        order.map((item) => <BasketItem key={item.id} {...item} />)
+        order.map((item) => (
+          <BasketItem
+            key={item.id}
+            {...item}
+            removeFromBasket={removeFromBasket}
+            incQuantity={incQuantity}
+            decQuantity={decQuantity}
+          />
+        ))
       ) : (
         <li className="collection-item">Корзина Пуста</li>
       )}
-      <li className="collection-item active" >Обшая цена {totalPrice} Lei Europene</li>
-      <i className='material-icons basket-close' onClick={handleBasketShow} >close</i>
+      <li className="collection-item active">Обшая цена {totalPrice} руб.</li>
+      <i className="material-icons basket-close" onClick={handleBasketShow}>
+        close
+      </i>
     </ul>
   )
 }
